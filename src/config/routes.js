9bcdb008b9f43808c89f938a909719d64e9eaace
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-10-25 19:16:53 -0300

    alterando url

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,7 +5,7 @@ const prefix = process.env.API_PREFIX + process.env.API_VERSION
 module.exports = async (app) => {
     console.info('Registrando rotas...')
 
-    app.use(`${prefix}/log`, log)   
+    app.use(`${prefix}/catalog`, log)   
 
     return app
 }
