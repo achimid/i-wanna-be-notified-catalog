d986ab4132146b33b159103a23bdb4df918898d6
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-10-31 21:19:44 -0300

    teste

--- src/log/log-controller.js
+++ src/log/log-controller.js
@@ -6,15 +6,21 @@ router.post('/', (req, res) => {
     res.send()
     
     const e = req.body
-    const log = `${e.uuid} | ${e.url} | ${e.extractedContent}`
+    const log = `${e.extractedContent[0]}`
+    // console.log(e.url)
+    console.log(log)
 
-    buffer.push(log)    
+    // buffer.push(log)    
+    // console.log(e.extractedContent.length > 0)
+    // if (e.extractedContent.length == 0) {
+    //     console.log(e)
+    // }
 })
 
-setInterval(() => {
-    console.log(buffer)
-    buffer = []
-}, 30000)
+// setInterval(() => {
+//     console.log(buffer)
+//     buffer = []
+// }, 30000)
 
 
 module.exports = router
